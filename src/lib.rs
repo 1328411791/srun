f307a8c8940730a06a4f5f36e2202dcdbc636f16
lihanyu <1328411791@qq.com>
2024-01-31 01:07:45 +0800

    fix: 修改

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,12 @@ pub use utils::get_ip_by_if_name;
 pub use utils::select_ip;
 pub use xencode::param_i;
 
-mod user;
-mod srun;
-mod xencode;
 mod file;
-mod utils;
 #[cfg(feature = "ureq")]
 mod http_client;
+mod srun;
+mod user;
+mod utils;
+mod xencode;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
